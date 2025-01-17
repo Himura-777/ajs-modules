3b2bc74dde4597209663ca8dc9650ba491fe58a9
Tuan Anh <tp.anhh7@gmail.com>
2025-01-17 22:55:53 +0300

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,15 @@
-module.exports = {};
+const {merge} = require('webpack-merge');
+
+const commonConfig = require('./webpack.common.config.js');
+
+const productionConfig = require('./webpack.production.config.js');
+
+const developmentConfig = require('./webpack.development.config.js');
+
+module.exports = function (env) {
+  if (env.development) {
+    return merge(commonConfig, developmentConfig);
+  } else {
+    return merge(commonConfig, productionConfig);
+  } 
+}
